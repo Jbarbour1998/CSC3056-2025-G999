1ba2c9fc4a800f9d25cb34c21b0ef3e2510ebb00
James Ballantine <jballantine02@qub.ac.uk>
2025-02-05 20:10:33 +0000

    Manual Tests

---
+++ manualTests/AccountTest.java
@@ -0,0 +1,22 @@
+package manualTests;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import model.Account;
+
+public class AccountTest {
+
+
+		public static void main(String[] args) throws Exception {
+
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+            // Parse the date string into a Date object
+            Date account_opening_date = dateFormat.parse("2024-01-30");
+			
+			Account testAccount = new Account("1234", "Mike", "Standard", account_opening_date);
+			
+			System.out.println(testAccount);
+		}	
+}
---
+++ manualTests/TransactionTest.java
@@ -0,0 +1,21 @@
+package manualTests;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import model.Transaction;
+
+public class TransactionTest {
+
+		public static void main(String[] args) throws Exception {
+
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+            // Parse the date string into a Date object
+            Date transaction_date = dateFormat.parse("2025-01-30");
+			
+            Transaction testTransaction = new Transaction("1234", 200.00, transaction_date);
+			
+			System.out.println(testTransaction);
+		}	
+}
