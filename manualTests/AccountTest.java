1bc1250248b09796149ff523b727f29abb3a81e4
James Ballantine <jballantine02@qub.ac.uk>
2025-02-06 19:26:12 +0000

    TODO 6 and tidy up utils class

--- manualTests/AccountTest.java
+++ manualTests/AccountTest.java
@@ -1,18 +1,16 @@
 package manualTests;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import model.Account;
+import utils.TestUtils;
 
 public class AccountTest {
 
 	public static void main(String[] args) throws Exception {
 
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-
 		// Parse the date string into a Date object
-		Date account_opening_date = dateFormat.parse("2024-01-30");
+		Date account_opening_date = TestUtils.parseDate("2024-01-30");
 
 		Account testAccount = new Account("1234", "Mike", "Standard", account_opening_date);
 
--- utils/TestUtils.java
+++ utils/TestUtils.java
@@ -1,5 +1,9 @@
 package utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TestUtils {
 
 	public static final String TEXT_COLOR_GREEN = "\u001B[32m"; // from
@@ -14,4 +18,15 @@ public class TestUtils {
 	public static void printTestFailed(String testCase) {
 		System.out.println(TEXT_COLOR_RED + "FAILED -- " + testCase + TEXT_COLOR_RESET);
 	}
+	
+	// Utility method to parse date from string
+    public static Date parseDate(String dateStr) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            return dateFormat.parse(dateStr);
+        } catch (ParseException e) {
+            System.out.println(TEXT_COLOR_RED + "Error parsing date: " + e.getMessage() + TEXT_COLOR_RESET);
+            return null; // Return null if parsing fails
+        }
+    }
 }
